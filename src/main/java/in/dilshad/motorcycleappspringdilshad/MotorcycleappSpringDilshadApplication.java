c06303392e97c44425ae6d863f2b8a778e785136
Dilshad21 <82450076+Dilshad21@users.noreply.github.com>
2021-06-15 16:07:30 +0530

    Initial commit

---
+++ src/main/java/in/dilshad/motorcycleappspringdilshad/MotorcycleappSpringDilshadApplication.java
@@ -0,0 +1,13 @@
+package in.dilshad.motorcycleappspringdilshad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorcycleappSpringDilshadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorcycleappSpringDilshadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dilshad/motorcycleappspringdilshad/MotorcycleappSpringDilshadApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dilshad.motorcycleappspringdilshad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorcycleappSpringDilshadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
